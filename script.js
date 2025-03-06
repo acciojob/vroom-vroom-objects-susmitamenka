63d95d331b9b71b3e057f1317ee0e2de14611323
susmitamenka <susmitaprasad73@gmail.com>
2025-03-06 22:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,23 @@ function Car(make, model) {}
 function SportsCar(make, model, topSpeed) {}
 
 // Do not change the code below
+class Car{
+	constructor(make,model){
+		this.make = make
+		this.model = model
+	}
+	getMakeModel(){
+		return(`${this.make},${this.model}`)
+	}
+}
+class SportsCar extends Car{
+	constructor(make,model,topSpeed){
+		super(make,model)
+		this.topSpeed = topSpeed
+	}
+	getTopSpeed(){
+		return(`${this.topSpeed}of sports car`)
+	}
+}
 window.Car = Car;
 window.SportsCar = SportsCar;
